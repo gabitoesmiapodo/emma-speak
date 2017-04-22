a59b49195fbb4e329d36dbef287b21229aa41d94
gabitoesmiapodo <development@iFrontend.local>
2017-04-22 02:55:22 -0300

    Few additions, and refactoring.

--- app/pages/free-writing/free-writing.component.ts
+++ app/pages/free-writing/free-writing.component.ts
@@ -10,19 +10,39 @@ import { SpeakConfig } from '../../shared/config/speech-config';
 
 export class FreeWritingComponent {
 
-    TTS = new TNSTextToSpeech();
-    speakConfig = new SpeakConfig;
-    speakOptions: SpeakOptions = this.speakConfig.speakOptionsDefault;
-    tmpSpeechText : string = '';
+    private TTS = new TNSTextToSpeech();
+    private speakConfig = new SpeakConfig;
+    private speakOptions : SpeakOptions = this.speakConfig.speakOptionsDefault;
+    private _isSpeaking : boolean = false;
+    private _tmpSpeechText : string = '';
 
     constructor(private page: Page) {
+
         page.actionBarHidden = true;
+
+    }
+
+    // getters / setters
+    get isSpeaking() : boolean {
+        return this._isSpeaking;
+    }
+
+    set isSpeaking(status : boolean) {
+        this._isSpeaking = status;
+    }
+
+    get tmpSpeechText() : string {
+        return this._tmpSpeechText;
+    }
+
+    set tmpSpeechText(theText : string) {
+        this._tmpSpeechText = theText;
     }
 
     /**
-     * Speak
+     * Checks if there's something to say
      */
-    emmaSpeak() : boolean {
+    isThereSomethingToSay() : boolean {
 
         if(typeof this.tmpSpeechText == undefined) {
             return false;
@@ -31,12 +51,55 @@ export class FreeWritingComponent {
             return false;
         }
         else {
-
-            // Convert to lowercase so it is correctly spoken (Android)
-            this.speakOptions.text = this.tmpSpeechText.toLowerCase();
-            this.TTS.speak(this.speakOptions);
-
             return true;
+        }
+
+    }
+
+    /**
+     * Convert to lowercase so it is correctly spoken (Android)
+     */
+    private fixAndroidUppercaseGlitch() : void {
+
+        if(this.page.android) {
+            this.speakOptions.text = this.tmpSpeechText.toLowerCase();
+        }
+
+    }
+
+    /**
+     * Check if the Speak Button is disabled
+     */
+    private isSpeakButtonDisabled() : boolean {
+        return this.isSpeaking || !this.isThereSomethingToSay();
+    }
+
+    /**
+     * Check if the Speak Button is enabled
+     */
+    private isSpeakButtonEnabled() : boolean {
+        return this.isThereSomethingToSay() && !this.isSpeaking;
+    }
+
+    /**
+     * Speak
+     */
+    emmaSpeak() : void {
+
+        if(this.isSpeaking) {
+            return;
+        }
+        else if(this.isSpeakButtonEnabled()) {
+
+            // I can't make the speakOptions callback work,
+            // so I will "fix" this with a timeout...
+            setTimeout(()=> {
+                this.isSpeaking = false;
+            }, 2000);
+
+            this.fixAndroidUppercaseGlitch();
+            this.isSpeaking = true;
+            this.TTS.speak(this.speakOptions);
 
         }
 
